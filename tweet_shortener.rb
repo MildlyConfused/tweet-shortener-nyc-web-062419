fcb072f94b33b05a85d15e3f8617a3b028235753
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-19 17:18:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,4 +30,4 @@ def word_subsituter(tweet)
   newTweet
 end
 
-word_subsituter("two four")
+word_subsituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")

fa83ce72c3d87f813e6c259ba4c99f2ec953e00c
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-19 17:22:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,4 +31,11 @@ def word_substituter(tweet)
   newTweet
 end
 
+
+
+
+
 word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
+
+
+

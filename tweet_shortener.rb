41d3788d71112359174902c7f6cc98df4104fda3
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-19 17:55:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -63,7 +63,4 @@ def shortened_tweet_truncator(tweet)
   else
     return shortTweet
   end
-    
 end
-
-puts shortened_tweet_truncator("GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!")

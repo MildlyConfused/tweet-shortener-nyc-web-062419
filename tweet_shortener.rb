d6351520a7215546ab3bb8354cfdfbbc374ffc8a
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-19 16:49:53 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,16 @@
 # Write your code here.
 def dictionary
   words = {
-    "hello" => 'hi'
-    "to, two, too" become '2' 
-    "for, four" become '4'
-    'be' becomes 'b'
-    'you' becomes 'u'
-    "at" becomes "@" 
-    "and" becomes "&"
+    "hello" => 'hi',
+    "to" => '2', 
+    "too" => '2',
+    "two" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
   }
 end
 
